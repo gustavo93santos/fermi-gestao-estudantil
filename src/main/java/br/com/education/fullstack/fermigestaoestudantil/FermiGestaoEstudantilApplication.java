4836abf72e27195c399c480cf0dbfcd51dd832ae
Gustavo Rafael dos Santos <gustavo93santos@gmail.com>
2024-04-18 22:29:45 -0300

    Commit Inicial

---
+++ src/main/java/br/com/education/fullstack/fermigestaoestudantil/FermiGestaoEstudantilApplication.java
@@ -0,0 +1,13 @@
+package br.com.education.fullstack.fermigestaoestudantil;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FermiGestaoEstudantilApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FermiGestaoEstudantilApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/education/fullstack/fermigestaoestudantil/FermiGestaoEstudantilApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.education.fullstack.fermigestaoestudantil;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FermiGestaoEstudantilApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
